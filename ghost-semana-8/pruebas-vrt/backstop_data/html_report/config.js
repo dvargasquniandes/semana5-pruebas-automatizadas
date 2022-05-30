89baf82783051f562c5a25bdd349b1b0db0d63c8
Andres Martin Cantor Urrego <andres.cantor@payulatam.com>
2022-05-29 21:51:57 -0500

    VRT

---
+++ ghost-semana-8/pruebas-vrt/backstop_data/html_report/config.js
@@ -0,0 +1,216 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Inicio_-_Ghost_version_4471_0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Inicio_-_Ghost_version_4471_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Inicio_-_Ghost_version_4471_0_document_0_default.png",
+        "label": "Inicio - Ghost version 4.47.1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/ghost/#/signin",
+        "referenceUrl": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.022291666666666668,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 42
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Home_-_Ghost_version_4471__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Home_-_Ghost_version_4471__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_-_Ghost_version_4471__0_document_0_default.png",
+        "label": "Home - Ghost version 4.47.1 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/",
+        "referenceUrl": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -35
+          },
+          "rawMisMatchPercentage": 13.519182825484766,
+          "misMatchPercentage": "13.52",
+          "analysisTime": 78
+        },
+        "diffImage": "../bitmaps_test/20220515-194649/failed_diff_backstop_default_Home_-_Ghost_version_4471__0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_About_-_Ghost_version_4471__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_About_-_Ghost_version_4471__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_About_-_Ghost_version_4471__0_document_0_default.png",
+        "label": "About - Ghost version 4.47.1 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/about/",
+        "referenceUrl": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/about/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Posts_-_Ghost_version_4471__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Posts_-_Ghost_version_4471__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Posts_-_Ghost_version_4471__0_document_0_default.png",
+        "label": "Posts - Ghost version 4.47.1 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/ghost/#/posts",
+        "referenceUrl": "http://ec2-54-145-214-45.compute-1.amazonaws.com:2369/ghost/#/posts",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.023125,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 46
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Inicio_-_Ghost_version_3420_0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Inicio_-_Ghost_version_3420_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Inicio_-_Ghost_version_3420_0_document_0_default.png",
+        "label": "Inicio - Ghost version 3.42.0",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/ghost/#/signin",
+        "referenceUrl": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.022291666666666668,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 52
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Home_-_Ghost_version_3420__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Home_-_Ghost_version_3420__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_-_Ghost_version_3420__0_document_0_default.png",
+        "label": "Home - Ghost version 3.42.0 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/",
+        "referenceUrl": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -51
+          },
+          "rawMisMatchPercentage": 12.854096709200805,
+          "misMatchPercentage": "12.85",
+          "analysisTime": 65
+        },
+        "diffImage": "../bitmaps_test/20220515-194649/failed_diff_backstop_default_Home_-_Ghost_version_3420__0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Tag_-_Ghost_version_3420__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Tag_-_Ghost_version_3420__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Tag_-_Ghost_version_3420__0_document_0_default.png",
+        "label": "Tag - Ghost version 3.42.0 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/tag/getting-started/",
+        "referenceUrl": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/tag/getting-started/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.1227924944812362,
+          "misMatchPercentage": "0.12",
+          "analysisTime": 30
+        },
+        "diffImage": "../bitmaps_test/20220515-194649/failed_diff_backstop_default_Tag_-_Ghost_version_3420__0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Author_-_Ghost_version_3420__0_document_0_default.png",
+        "test": "../bitmaps_test/20220515-194649/backstop_default_Author_-_Ghost_version_3420__0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Author_-_Ghost_version_3420__0_document_0_default.png",
+        "label": "Author - Ghost version 3.42.0 ",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/author/ghost/",
+        "referenceUrl": "http://ec2-52-200-112-117.compute-1.amazonaws.com:2369/author/ghost/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 1.591887417218543,
+          "misMatchPercentage": "1.59",
+          "analysisTime": 38
+        },
+        "diffImage": "../bitmaps_test/20220515-194649/failed_diff_backstop_default_Author_-_Ghost_version_3420__0_document_0_default.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
